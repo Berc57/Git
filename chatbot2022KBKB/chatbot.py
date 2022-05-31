b14d125a992a8dbf05718cdf5442394102544f3a
Keing <0507bercel@gmail.com>
2022-06-01 00:59:06 +0200

    chatbotproject2022KBKB

---
+++ chatbot2022KBKB/chatbot.py
@@ -0,0 +1,17 @@
+import time
+
+x = "."
+
+f = open("szoveg.txt","r",encoding="utf-8")
+while x != "kilépés":
+    x = input("->")
+    if "szia" == x:
+        time.sleep(1)
+        print("Neked is szia!!")
+    elif "helo" == x:
+        time.sleep(1)
+        print("Helló")
+    else:
+        time.sleep(1)
+        print("Tessék?")
+        
