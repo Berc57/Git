b517fae32be556fad113f910e659a11c4ab4d996
Keing <0507bercel@gmail.com>
2022-06-05 22:21:28 +0200

    fileolvasasbolproba

--- chatbot2022KBKB/chatbot.py
+++ chatbot2022KBKB/chatbot.py
@@ -3,14 +3,25 @@ import time
 x = "."
 
 f = open("szoveg.txt","r",encoding="utf-8")
+
+szoveglista = []
+
+for sor in f:
+    szoveglista.append(sor.strip().split(";"))
+    
+print(szoveglista)
+
 while x != "kilépés":
-    x = input("->")
+    x = input("-> ")
     if "szia" == x:
         time.sleep(1)
-        print("Neked is szia!!")
-    elif "helo" == x:
+        print(szoveglista[0][0])
+    elif "helo"== x:
         time.sleep(1)
-        print("Helló")
+        print(szoveglista[0][1])
+    elif "jó" in x:
+        time.sleep(1)
+        print(szoveglista[0][2])
     else:
         time.sleep(1)
         print("Tessék?")
